f5b7664df57efa7256e7bd651ce0a2ce89af0b99
pranavbhuchhada <pranavbhuchhada@gmail.com>
2020-05-15 13:39:18 +0530

    React Router

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay,
     CardTitle } from 'reactstrap';
+// import DishDetail from './DishdetailComponent';
+function RenderMenuItem ({dish, onClick}) {
+    return (
+        <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+                <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
 
-    function RenderMenuItem ({dish, onClick}) {
+const Menu = (props) => {
+
+    const menu = props.dishes.map((dish) => {
         return (
-            <Card
-                onClick={() => onClick(dish.id)}>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
-            </Card>
-        );
-    }
-
-    const Menu = (props) => {
-
-        const menu = props.dishes.map((dish) => {
-            return (
-                <div className="col-12 col-md-5 m-1"  key={dish.id}>
-                    <RenderMenuItem dish={dish} onClick={props.onClick} />
-                </div>
-            );
-        });
-
-        return (
-            <div className="container">
-                <div className="row">
-                    {menu}
-                </div>
+            <div className="col-12 col-md-5 m-1"  key={dish.id}>
+                <RenderMenuItem dish={dish} />
             </div>
         );
-    }
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                {menu}
+            </div>
+            {/* <DishDetail dish={props.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> */}
+        </div>
+    );
+}
 
 export default Menu;
