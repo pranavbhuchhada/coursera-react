68ea99e28ab81f94173df632be2edca23af92354
pranavbhuchhada <pranavbhuchhada@gmail.com>
2020-05-17 11:39:06 +0530

    React Redux Forms Revisited

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal,ModalBody,ModalHeader, Row, Label, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import {LocalForm, Control, Errors} from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -129,8 +130,25 @@ function RenderComments({comments, addComment, dishId}) {
     );
 }
 const  DishDetail = (props) => {
-    let dish = props.dish;
-    if(dish != null){
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) {
         return (
             <div className="container">
             <div className="row">
